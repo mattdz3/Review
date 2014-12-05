9b1464629e486b6561689300ffb57d56b5a3b955
Matt Dougherty <mattz325@gmail.com>
2014-12-04 21:29:46 -0500

    slidr working

--- app/scripts/fullReviewView.js
+++ app/scripts/fullReviewView.js
@@ -17,7 +17,23 @@ var FullReviewView = Parse.View.extend({
 	render: function() {
 		var renderTemplate = this.template(this.model.attributes)
 		this.$el.html(renderTemplate);
-		slidr.create('slidr-id').start();
+
+		slidr.create('slidr-id', {
+		  after: function(e) { console.log('in: ' + e.in.slidr); },
+		  before: function(e) { console.log('out: ' + e.out.slidr); },
+		  breadcrumbs: true,
+		  controls: 'border',
+		  direction: 'horizontal',
+		  fade: true,
+		  keyboard: true,
+		  overflow: false,
+		  pause: false,
+		  theme: '#222',
+		  timing: { 'cube': '0.5s ease-in' },
+		  touch: true,
+		  transition: 'linear'
+		}).start();	
+
 		return this;
 	},
 
