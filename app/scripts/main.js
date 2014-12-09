0f9ab05b74d7d8c3cf08c3948e024b46b65f6057
Matt Dougherty <mattz325@gmail.com>
2014-12-09 12:08:49 -0500

    added team slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,6 +13,22 @@ $('.logout').click(function() {
 	alert('user has logged out!')
 });
 
-$(document).ready(function(){
-	$("#sticky").sticky({topSpacing: 0});
+var mainSlider = slidr.create('img-slidr', {
+	// after: function(e) { console.log('in: ' + e.in.slidr); },
+	// before: function(e) { console.log('out: ' + e.out.slidr); },
+	breadcrumbs: true,
+	controls: 'border',
+	direction: 'h',
+	fade: true,
+	keyboard: true,
+	overflow: false,
+	pause: false,
+	theme: '#222',
+	timing: { 'linear': '.4s ease-in' },
+	touch: true,
+	transition: 'linear'
 });
+
+mainSlider.add('h', ['one', 'two', 'three', 'one'])
+
+mainSlider.auto();
